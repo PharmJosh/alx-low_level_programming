63946f909550a9e19729bef7e7b1778544b1927a
PharmJosh <alabijsh@gmail.com>
2022-12-09 14:34:41 -0800

    coming up

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - main function
  *
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
