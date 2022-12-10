c6ea81f49bbe02620eff0316c2fe3ea85a5997fc
PharmJosh <alabijsh@gmail.com>
2022-12-10 03:31:02 -0800

    ready!

--- 0x00-hello_world/6.size.c
+++ 0x00-hello_world/6.size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - main function
+ * main - Entry point
  *
- * Return: 0 on success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,7 +12,6 @@ int main(void)
 	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
-
 	return (0);
 
 }
