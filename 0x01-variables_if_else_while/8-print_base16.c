04ba89696c69f0ff255bb41bafe8aed9e66c0ccb
PharmJosh <alabijsh@gmail.com>
2022-12-12 17:59:29 -0800

    print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int s = 0, e = 9;
+
+	while (s <= e)
+	{
+		putchar(s + '0');
+		s++;
+	}
+
+	s = 97;
+	e = 102;
+
+	while (s <= e)
+	{
+		putchar(s);
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
