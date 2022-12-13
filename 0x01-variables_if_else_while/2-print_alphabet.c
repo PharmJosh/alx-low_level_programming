a8e53932aca5b5610d5323ee6b10bfab8fcc8897
PharmJosh <alabijsh@gmail.com>
2022-12-12 17:01:09 -0800

    print alphaber

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int s = 97, e =122;
+
+	while (s <= e)
+	{
+		putchar(s);
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
