921ac470c487f8c09e76c63e0d5ff8748e3e5f13
PharmJosh <alabijsh@gmail.com>
2022-12-12 17:50:21 -0800

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int s = 97, e = 122;
+
+	while (e >= s)
+	{
+		putchar(e);
+		e--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
