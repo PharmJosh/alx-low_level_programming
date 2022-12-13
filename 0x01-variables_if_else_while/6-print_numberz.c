aa162b671fea6b90dd38bdf5f19f99437c44422e
PharmJosh <alabijsh@gmail.com>
2022-12-12 17:44:49 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int s = 0, e = 9;
+
+	while (s <= e)
+	{
+		putchar(s + '0');
+		s++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
