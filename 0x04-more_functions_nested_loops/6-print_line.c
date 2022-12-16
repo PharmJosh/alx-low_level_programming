fe67565793910a34bc4a7629c268e4bfb2babf31
PharmJosh <alabijsh@gmail.com>
2022-12-16 12:34:15 -0800

    create a new line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: line length
+ *
+ * Return: nothing on success
+ */
+void print_line(int n)
+{
+	int count = 0;
+
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		while (count < n)
+		{
+			putchar('_');
+			count++;
+		}
+		putchar('\n');
+	}
+}
